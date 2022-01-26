bed2789604d75af981e3dc741ec563394b82c3ca
dimitri.liberman <dimitri.liberman@cgi.com>
2022-01-26 14:26:45 -0800

    Created branch to submit question to nrwl

---
+++ libs/lib-test/src/lib/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'storybook-button',
+  templateUrl: './button.component.html',
+  styleUrls: [
+    './button.component.scss',
+    // '../../../../../apps/storybooktest/src/styles.scss',
+  ],
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
